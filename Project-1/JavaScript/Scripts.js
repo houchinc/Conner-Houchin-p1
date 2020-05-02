22ae17ba4b5b77398e740a489e27cfcf38b74576
houchinc <60678107+houchinc@users.noreply.github.com>
2020-05-02 16:06:59 -0500

    Project 1 submitted

---
+++ Project-1/JavaScript/Scripts.js
@@ -0,0 +1,16 @@
+var author = 'Conner Houchin';
+var authorEL = document.getElementById('author');
+var componentScore = '8/10';
+var componentScoreEL = document.getElementById('componentsScore');
+var mechanicsScore = '10/10';
+var mechanicsScoreEL = document.getElementById('mechanicsScore');
+var themeScore = '9/10';
+var themeScoreEL = document.getElementById('themeScore');
+var finalScore ='9.5/10';
+var finalScoreEL = document.getElementById('finalScore');
+
+authorEL.textContent = author;
+componentScoreEL.textContent = componentScore;
+mechanicsScoreEL.textContent = mechanicsScore;
+themeScoreEL.textContent = themeScore;
+finalScoreEL.textContent = finalScore;
